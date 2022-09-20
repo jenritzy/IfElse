5f308986be564261c254a49f00c3b798f42f94b2
jenritzy <Jenritzy@outlook.com>
2022-09-20 13:29:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "IfElse";
+            Console.Write("Please enter a number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Thanks. Now enter a letter:");
+            char letter = Convert.ToChar(Console.ReadLine());
+
+            if(num>=6)
+            { Console.WriteLine("\nNumber exceeds 5");
+                if(letter == 'C')
+                { Console.WriteLine("Letter is 'C'");
+                }
+            }
+            else
+            { Console.WriteLine("\nNumber is 5 or less");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
